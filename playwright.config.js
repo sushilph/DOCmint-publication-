67854351a35d024f1f28a6a1323c9881995c7920
sushilph <sushilphulara1@gmail.com>
2026-04-28 16:15:49 +0545

    chnages on yml file

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@
 import { defineConfig, devices } from '@playwright/test';
 require('dotenv').config(); // Load environment variables from .env file
 
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
